d2769df10f83fc959b1bc2156d7a44d2970752cb
Halal-Lab <muhaminat@gmail.com>
2022-06-04 18:55:59 +0100

    m

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -6,6 +6,7 @@ import {BsWhatsapp} from 'react-icons/bs'
 // import {useRef} from 'react'
 // import emailjs from 'emailjs-com'
 
+
 const contact = () => {
   // const form = useRef();
 
@@ -18,38 +19,31 @@ const contact = () => {
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2>Sold Already?</h2>
 
       <div className="container contact__container">
+        
+      <h4> I'm currently looking for new opportunities, 
+        so my inbox is always open. Have a question or you 
+        just want to say hi? feel free to reach out to me
+      </h4>
+      
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon' />
-            <h4>Email</h4>
-            <h5>muhaminat01@gmail.com</h5>
-            <a href="mailto:muhaminat01@gmail.com">Send a Message</a>
+            <a href="mailto:muhaminat01@gmail.com"></a>
           </article>
 
           <article className="contact__option">
             <FaTwitter className='contact__option-icon'/>
-            <h4>Twitter</h4>
-            <h5>Muhammed Bliss</h5>
-            <a href="https://twitter.com/simply_meenat" target='_blank' rel="noreferrer" >Send a Message</a>
+            <a href="https://twitter.com/simply_meenat" target='_blank' rel="noreferrer" ></a>
           </article>
 
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon'/>
-            <h4>WhatsApp</h4>
-            <h5>+2348142974615</h5>
-            <a href="http://api.whatsapp.com/send?phone+2348142974615" target='_blank' rel="noreferrer">Send a Message</a>
+            <a href="http://api.whatsapp.com/send?phone+2348142974615" target='_blank' rel="noreferrer"></a>
           </article>
         </div>
-
-        <form action="">
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type="text" name='email' placeholder='Your Email' required/>
-          <textarea name="message" id=""  rows="7" placeholder='Your Message' required></textarea>
-           <button type='submit' className='btn btn-primary'>Send Message</button>
-        </form>
       </div>
     </section>
   )
